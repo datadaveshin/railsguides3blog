893a83fbb73a35f85f61aaba583a8fea77982e95
David Shin <davidshin444@gmail.com>
2018-06-11 18:21:23 -0600

    Init commit: rails new ...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsguides3::Application.config.secret_token = '3161688913715a6e6eb6b0115e2c98b93c83abe66826d0e141bd03dd048f017411a6f2f613ec51ac6f84a87dc35bf31b997f0549e49c8283442b472f2aa520a8'
